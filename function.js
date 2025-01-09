aaf876ccfbd41d998f0a7446c331797ab51d9805
Kauan913-ux <kauan914ka@gmail.com>
2025-01-09 16:29:45 -0300

    function

---
+++ function.js
@@ -0,0 +1,20 @@
+function saudacao () {
+    console.log('ola');
+}
+function sum(a,b) {
+    return a + b;
+}
+let resultado = sum(50, 10);
+console.log (resultado); // 60
+
+function decrease(a,b){
+    return a - b;
+}
+let result = decrease(29, 10);
+console.log(result); //19
+
+function nothing(a,b){
+    return a - b;
+}
+let res = nothing(0,0);
+console.log(res); //0
