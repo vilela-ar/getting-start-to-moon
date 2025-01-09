eed6f452a909e6f390554518bf953494ab97182e
Kauan913-ux <kauan914ka@gmail.com>
2025-01-09 16:53:19 -0300

    .\function.js

--- function.js
+++ function.js
@@ -6,3 +6,13 @@ function sum(a,b) {
 }
 let resultado = sum(50, 10);
 console.log (resultado); // 60
+let result = decrease(29, 10);
+console.log(result); //19
+
+function decrease(a,b){
+    return a - b;
+}
+    let result = decrease(29, 10);
+    console.log(result); //19
+
+    
