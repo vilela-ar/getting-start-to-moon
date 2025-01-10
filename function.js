c3c0118b6dae688a4fcc1b70f2c211abad21f030
Kauan913-ux <kauan914ka@gmail.com>
2025-01-10 10:07:52 -0300

    adicionaronothing

--- function.js
+++ function.js
@@ -1,21 +1,21 @@
-function saudacao () {
+function saudacao () { 
     console.log('ola');
 }
 saudacao();
 function sum(a,b) {
     return a + b;
 }
-let resultado = sum(50, 10);
+let resultado = sum(50, 10); //adicionar
 console.log (resultado); // 60
 
 function decrease(a,b){
     return a - b;
 }
-let result = decrease(29, 10);
+let result = decrease(29, 10); // subtrair
 console.log(result); //19
 
 function nothing(a,b){
-    return a - b;
+   //sem retorno
 }
-let res = nothing(0,0);
-console.log(res); //0
+let res = nothing(); 
+console.log(res);
