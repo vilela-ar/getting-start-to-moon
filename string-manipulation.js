2b1f65d3b54121b780dc18a0ff9226417e2ea340
Kauan913-ux <kauan914ka@gmail.com>
2025-01-10 20:57:42 -0300

    fix: espacos

--- string-manipulation.js
+++ string-manipulation.js
@@ -2,10 +2,7 @@ function capitalizeFirstLetter(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);  // str charAt pega o primeiro caractere da str 
 }                                                       // .toUpperCase deixa a primeira letra em maiuscula
 // str.slice pega o resto dos caracteres da str a partir da 2 letra
-
 console.log(capitalizeFirstLetter("javascript")); 
-
-
 
 function countWords(string) {
     const palavras = string.trim().split(/\s+/); //.trim remove espaços em branco do inicio ao final da string
