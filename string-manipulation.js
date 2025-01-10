c3babdcc1d14f286d4c93d1d2620454225fd193f
Kauan913-ux <kauan914ka@gmail.com>
2025-01-10 20:58:38 -0300

    feat: Capitalizar o primeiro caractere da string (#5)
    * capitalizar o primeiro caractere da string
    
    * contar palavras
    
    * substituir palavras da frase
    
    * criar um palindromo que pode ser lida da mesma forma de tras pra frente
    
    * pegar apenas o gmail.com do gmail inteiro
    
    * fix: espacos
    
    * fix: indentacao

---
+++ string-manipulation.js
@@ -0,0 +1,40 @@
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);  // str charAt pega o primeiro caractere da str 
+}                                                       // .toUpperCase deixa a primeira letra em maiuscula
+// str.slice pega o resto dos caracteres da str a partir da 2 letra
+console.log(capitalizeFirstLetter("javascript")); 
+
+function countWords(string) {
+    const palavras = string.trim().split(/\s+/); //.trim remove espaços em branco do inicio ao final da string
+    //.split(/\s+/) \s+ considelace(rra qualquer quantidade de espaços consecutivos.,  Divide a string usando espaços em branco (incluindo múltiplos espaços) como delimitador
+    return palavras.length
+}
+console.log(countWords("Eu gosto de programar em JavaScript")); // Deve imprimir 6
+
+
+
+function replaceWord(sentence, oldWord, newWord) {
+    texto_modificado = sentence.replace(oldWord, newWord)
+    return texto_modificado
+}
+console.log(replaceWord("Eu gosto de JavaScript", "JavaScript", "Python"));
+// Deve imprimir "Eu gosto de Python"
+
+
+function isPalindrome(str) {
+    const strLimpa = str.replace(/\s+/g, '').toLowerCase();  // Remove espaços e converte para minúsculas
+    const strInvertida = strLimpa.split('').reverse().join('');   // Inverte a string
+    return strLimpa === strInvertida; //  // Verifica se a string limpa é igual à versão invertida
+}
+console.log(isPalindrome("arara")); // Deve imprimir true
+console.log(isPalindrome("JavaScript")); // Deve imprimir false
+
+
+
+function getDomain(email) {
+    return (email.substring(8, 18));
+
+}
+
+
+console.log(getDomain("usuario@gmail.com")); // Deve imprimir "gmail.com"
