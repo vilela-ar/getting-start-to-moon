73c6653290b4dba736626aea997b385704ce5b62
Kauan913-ux <kauan914ka@gmail.com>
2025-01-10 10:31:42 -0300

    contar palavras

--- string-manipulation.js
+++ string-manipulation.js
@@ -4,3 +4,12 @@ function capitalizeFirstLetter(str) {
 // str.slice pega o resto dos caracteres da str a partir da 2 letra
 
 console.log(capitalizeFirstLetter("javascript")); 
+
+function countWords(string) {
+    const palavras = string.trim().split(/\s+/); //.trim remove espaços em branco do inicio ao final da string
+    //.split(/\s+/) \s+ considera qualquer quantidade de espaços consecutivos.,  Divide a string usando espaços em branco (incluindo múltiplos espaços) como delimitador
+    return palavras.length
+}
+
+// Teste a função:
+console.log(countWords("Eu gosto de programar em JavaScript")); // Deve imprimir 6
